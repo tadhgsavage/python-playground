4d8fac608e6823f527e542b44fd23d24ee65a3a3
tadhgsavage <tadhgsavage@gmail.com>
2025-09-03 15:14:45 +0100

    Additions to file

--- python_test.py
+++ python_test.py
@@ -39,9 +39,70 @@ print (message.strip())
 website = ("https://apple.com")
 print(website.removeprefix("https://"))
 
-
+# Remove suffix
 website = ("https://apple.com")
 print(website.removesuffix(".com"))
 
+# Remove both prefix and suffix 
 webiste = ("https://apple.com")
 print(website.removeprefix("https://").removesuffix(".com"))
+
+# Multiple assignment with numbers
+x, y, z = 1, 2, 3
+print(x,y,z)
+
+# Constants 
+MAX_CONECTIONS = 5_000
+
+# Lists 
+cars = ['bugatti', 'ford', 'ferrari']
+print(cars)
+print(cars[0])
+print(cars[0].title())
+
+# Accessing elements on the last of a list
+print(cars[-1].title())
+
+# Now putting into an f string
+cars = ['bugatti', 'ford', 'ferrari']
+message = (f'My favourite car is a {cars[-1].title()}')
+print(message)
+
+# Modifying elements in a list
+cars = ['bugatti', 'ford', 'ferrari']
+cars[0] = 'bentley'
+print(cars)
+
+# Adding an element to a list
+cars.append('citroen')
+print(cars)
+
+# One can start with an empty list and just append all items in
+cars = []
+cars.append("ford")
+cars.append("bugatti")
+cars.append("fiat")
+print(cars)
+
+# One can also insert an elemet into a list
+cars.insert(1, "bmw")
+print(cars)
+
+# Removing an element from a list using the del statement
+del cars[0]
+print(cars)
+
+# Removing items using the pop method
+popped_car = cars.pop()
+print(cars)
+print(popped_car)
+
+# Making this useful
+cars = ["fiat", 'ford', 'bentley', 'ferrari']
+last_car = cars.pop(1)
+message = (f"The last car i owned was a, {last_car.title()}.")
+print(message)
+
+
+
+
