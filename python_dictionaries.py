dea5307e5dd43dc73682ef9027a8fe647eae3f99
tadhgsavage <tadhgsavage@gmail.com>
2025-09-10 13:53:56 +0100

    Using sorted and loops in dicitonaries

--- python_dictionaries.py
+++ python_dictionaries.py
@@ -1,12 +1,29 @@
 favourite_languages = {
-    'toby': 'python',
+    'toby': 'java',
     'chiara': 'html',
-    'tadhg': 'rust',
-    'jamie': 'css',
+    'tadhg': 'python',
     'dan': 'golang',
-    }
+    'jemma': 'c',
+}
 
-freinds = ['tadhg', 'chiara']
+favourite_friends = ['tadhg', 'chiara']
 
-for key in favourite_languages.keys():
-    print(key.title())
+if 'erin' not in favourite_friends:
+    print('Erin, please take our poll.')
+
+# One can also loop thorugh a dictionaries keys in a particular order
+
+favourite_languages = {
+    'toby': 'python',
+    'tadhg': 'html',
+    'euan': 'java',
+    'chiara': 'css',
+}
+
+for key in (sorted(favourite_languages.keys())):
+    print(f'Hi, {key.title()}')
+
+print('The following languages have been mentioned:')
+for lanhuage in (sorted(favourite_languages.values())):
+    print(lanhuage.title())
+
