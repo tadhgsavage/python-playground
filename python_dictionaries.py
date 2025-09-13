14ab24436bf200d950a6504579c36c938afd4b67
tadhgsavage <tadhgsavage@gmail.com>
2025-09-13 16:26:24 +0100

    Creation of dictionaries inside of dictionaries

--- python_dictionaries.py
+++ python_dictionaries.py
@@ -150,7 +150,27 @@ for names, pizzas in favourite_pizzas.items():
         print(pizza.title())
     
 
+# Creating a dicitonary inside of a dicitonary
 
+users = {
+    'tsavage': {
+        'first': 'tadhg',
+        'last': 'savage',
+        'location': 'kent',
+    },
+    'cpresaghi': {
+        'first': 'chiara',
+        'last': 'presaghi',
+        'location': 'rome',
+    },
+}
+
+for username, user_info in users.items():
+    print(f'Username: {username}')
+    full_name = f'{user_info['first']} {user_info['last']}'
+    location = user_info['location']
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
 
 
 
