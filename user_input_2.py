17bda013b11a13230ac2f28b15e86560eeb00c8c
tadhgsavage <tadhgsavage@gmail.com>
2025-09-14 18:42:19 +0100

    Addition of creating flags to manage while loops

--- user_input_2.py
+++ user_input_2.py
@@ -13,7 +13,39 @@ if number % 2 == 0:
 else:
     print(f'Sorry chuck your number of {number} is no good.')
 
-current_number = 1
+
+# Lets create a while loop to make a number counter up to 5
+current_number = 5
+# Creation of the while loop
 while current_number <= 5:
     print(current_number)
-    current_number = current_number + 1
+    # Add the value of 1 to current number inside of the while loop
+    current_number += 1
+
+
+# We can also get a program to keep running with a while loop 
+prompt = ('\nGive me a sentence and I shall repeat it back to you: ')
+prompt += ("\nEnter 'quit' and the program will stop running.")
+
+message = ('')
+while message != ('quit'):
+    message = input(prompt)
+    print(message)
+
+
+# This while loop works but when the user inputs quit the quit message is still displayed, which might not be ideal
+# This can, however, be fixed by a simple if statement
+
+prompt = "\nInput a message of your choice and this program will print it back to you"
+prompt += "\nWrite 'quit' to quit the program: "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
+
+
+
+
+
